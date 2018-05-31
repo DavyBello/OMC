7dda9fa704a052c079fb7cc3a9e90b5ea591ec6d
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-05-31 21:20:03 +0100

    initial commit

---
+++ pages/b.js
@@ -0,0 +1,9 @@
+import Dashboard from '../components/MaterialDashboard/Dashboard'
+// import BS from 'assets/jss/material-dashboard-react/buttonStyle'
+
+export default () => (
+    <Dashboard />
+    // <div>
+    //   a
+    // </div>
+)
